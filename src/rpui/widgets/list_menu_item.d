0be7b41bdc9908c4f06bef94ad391d2c94252be3
Andrey Kabylin <andrey@kabylin.ru>
2018-09-04 22:33:56 +0700

    render submenu arrow

--- src/rpui/widgets/list_menu_item.d
+++ src/rpui/widgets/list_menu_item.d
@@ -7,11 +7,45 @@ module rpui.widgets.list_menu_item;
 
 import gapi;
 import basic_types;
+import math.linalg;
 
 import rpui.widgets.button;
+import rpui.widgets.list_menu;
+import rpui.render_objects;
 
 final class ListMenuItem : Button {
     @Field string shortcut = "";
+
+    private ListMenu menu = null;
+    private BaseRenderObject submenuArrowRenderObject;
+    private vec2 submenuArrowOffset;
+
+    protected override void onCreate() {
+        super.onCreate();
+
+        const states = ["Leave", "Enter", "Click"];
+        submenuArrowRenderObject = renderFactory.createQuad(style, states, "submenuArrow");
+
+        with (manager.theme.tree) {
+            submenuArrowOffset = data.getVec2f(style ~ ".submenuArrowOffset");
+        }
+    }
+
+    protected override void onPostCreate() {
+        super.onPostCreate();
+
+        if (children.empty)
+            return;
+
+        menu = cast(ListMenu)(children.front);
+
+        if (menu is null)
+            return;
+
+        menu.visible = false;
+        menu.focusable = false;
+        manager.moveWidgetToFront(menu);
+    }
 
     this(in string style = "ListItem", in string iconsGroup = "icons") {
         super(style, iconsGroup);
@@ -22,5 +56,10 @@ final class ListMenuItem : Button {
 
     override void render(Camera camera) {
         super.render(camera);
+
+        if (menu !is null) {
+            const arrowPosition = absolutePosition + vec2(size.x - submenuArrowRenderObject.scaling.x, 0);
+            renderer.renderQuad(submenuArrowRenderObject, state, arrowPosition + submenuArrowOffset);
+        }
     }
 }
