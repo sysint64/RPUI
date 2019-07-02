433e795cf6e79ee43c036dab02bc6330d41c1843
Andrey Kabylin <andrey@kabylin.ru>
2019-07-02 15:36:31 +0700

    expose sdl to platform_sdl.d

--- src/rpui/platform.d
+++ src/rpui/platform.d
@@ -14,148 +14,29 @@ import rpui.events;
 import rpui.events_observer;
 import rpui.input;
 
-void platformShowSystemCursor() {
-    SDL_ShowCursor(SDL_ENABLE);
-}
-
-void platformHideSystemCursor() {
-    SDL_ShowCursor(SDL_DISABLE);
-}
-
-void platformSetMousePosition(void* window, in int x, in int y) {
-    SDL_WarpMouseInWindow(cast(SDL_Window*) window, x, y);
-}
-
-float platformGetTicks() {
-    return SDL_GetTicks();
-}
-
-bool platformEventLoop(EventsObserver events) {
-    SDL_Event event;
-
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            return false;
-        }
-        else if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
-            events.notify(WindowResizeEvent(event.window.data1, event.window.data2));
-        }
-        else if (event.type == SDL_MOUSEMOTION) {
-            const button = createMouseButtonFromSdlState(event.motion.state);
-            events.notify(MouseMoveEvent(event.motion.x, event.motion.y, button));
-        }
-        else if (event.type == SDL_MOUSEBUTTONDOWN) {
-            const button = createMouseButtonFromButtonSdlEvent(event);
-            events.notify(MouseDownEvent(event.button.x, event.button.y, button));
-
-            switch (event.button.clicks) {
-                case 2:
-                    events.notify(DblClickEvent(event.button.x, event.button.y, button));
-                    break;
-
-                case 3:
-                    events.notify(TripleClickEvent(event.button.x, event.button.y, button));
-                    break;
-
-                default:
-                    // Ignore
-            }
-        }
-        else if (event.type == SDL_MOUSEBUTTONUP) {
-            const button = createMouseButtonFromButtonSdlEvent(event);
-            events.notify(MouseUpEvent(event.button.x, event.button.y, button));
-        }
-        else if (event.type == SDL_MOUSEWHEEL) {
-            events.notify(MouseWheelEvent(event.wheel.x, event.wheel.y));
-        }
-        else if (event.type == SDL_TEXTINPUT) {
-            const ch = fromStringz(dtext(event.text.text[0..4]).ptr)[0];
-            events.notify(TextEnteredEvent(ch));
-        }
-        else if (event.type == SDL_KEYDOWN) {
-            try {
-                const key = to!(KeyCode)(to!(int)(event.key.keysym.sym));
-                setKeyPressed(key, true);
-                events.notify(KeyPressedEvent(key));
-            } catch (Exception e) {
-            }
-        }
-        else if (event.type == SDL_KEYUP) {
-            try {
-                const key = to!(KeyCode)(to!(int)(event.key.keysym.sym));
-                events.notify(KeyReleasedEvent(key));
-                setKeyPressed(key, false);
-            } catch (Exception e) {
-            }
-        }
-    }
-
-    return true;
-}
-
-void platformSwapWindow(void* window) {
-    SDL_GL_SwapWindow(cast(SDL_Window*) window);
-}
-
-void platformGapiDeleteContext(void* context) {
-    SDL_GL_DeleteContext(context);
-}
-
-void platformDestroyWindow(void* window) {
-    SDL_DestroyWindow(cast(SDL_Window*) window);
-}
-
-void platformShutdown() {
-    SDL_Quit();
-    TTF_Quit();
-}
-
-void platformInit() {
-    // Load shared libraries
-    DerelictGL3.load();
-    DerelictSDL2.load();
-    DerelictSDL2Image.load();
-    DerelictSDL2TTF.load();
-
-    // Init
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        throw new Error("Failed to init SDL");
-
-    if (TTF_Init() < 0)
-        throw new Error("Failed to init SDL TTF");
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetSwapInterval(2);
-}
-
 struct Window {
     void* handle;
     void* gapiContext;
 }
 
-Window platformCreateWindow(in string title, uint width, uint height) {
-    auto window = SDL_CreateWindow(
-        toStringz(title),
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        width,
-        height,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-    );
+extern(C) void platformShowSystemCursor();
 
-    if (window == null)
-        throw new Error("SDL Error: " ~ to!string(SDL_GetError()));
+extern(C) void platformHideSystemCursor();
 
-    auto glContext = SDL_GL_CreateContext(window);
+extern(C) void platformSetMousePosition(void* window, in int x, in int y);
 
-    if (glContext == null)
-        throw new Error("SDL Error: " ~ to!string(SDL_GetError()));
+extern(C) float platformGetTicks();
 
-    SDL_GL_SwapWindow(window);
-    DerelictGL3.reload();
+extern(C) bool platformEventLoop(EventsObserver events);
 
-    return Window(window, glContext);
-}
+extern(C) void platformSwapWindow(void* window);
+
+extern(C) void platformGapiDeleteContext(void* context);
+
+extern(C) void platformDestroyWindow(void* window);
+
+extern(C) void platformShutdown();
+
+extern(C) void platformInit();
+
+extern(C) Window platformCreateWindow(in string title, uint width, uint height);
